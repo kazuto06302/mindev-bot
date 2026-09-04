ef966df38bd50a79be1aedfaf239a6f330e0dae1
kazut0_ <158010474+kazuto06302@users.noreply.github.com>
2026-09-05 07:26:07 +0900

    Integrate Flask web server for bot status
    Added a Flask web server to provide a status endpoint.

--- mindev.py
+++ mindev.py
@@ -2,6 +2,19 @@ import os
 import discord
 from discord.ext import commands
 
+// web server
+from threading import Thread
+from flask import Flask
+app = Flask("")
+@app.route("/")
+def home():
+    return "Bot is active!"
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+Thread(target=run_web).start()
+
+
 intents = discord.Intents.default()
 
 bot = commands.Bot(
